7ac95504cd07853be56ec179c15ddd9eff9042ee
khaledhajij <hajij94@gmail.com>
2025-10-02 13:23:33 +0300

    chore(app): update demo page number to 106
    - Change \pageNumber\ in \App.tsx\ from 594 to 106 for local testing/demo.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { MushafPage, MushafPageProvider } from './index';
 const MushafPageConsumer: React.FC = () => <MushafPage />;
 
 const App: React.FC = () => (
-  <MushafPageProvider initialIsTwoPagesView hasBorder pageNumber={594} dataId="quran-hafs">
+  <MushafPageProvider initialIsTwoPagesView hasBorder pageNumber={106} dataId="quran-hafs">
     <MushafPageConsumer />
   </MushafPageProvider>
 );
