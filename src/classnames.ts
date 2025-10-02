d68a65ad7b7063c9e382df20fb5416bc7832c2d7
khaledhajij <hajij94@gmail.com>
2025-10-02 15:38:44 +0300

    refactor(classnames): prune unused mappings and variants
    - Remove ChapterHeader, MushafReader, Line, and Page maps from registry.
    - Trim ChapterIconContainer size keys and unused MushafWord mushaf-* variants.
    - Keep only active ReadingView and VerseText.highlighted entries.

--- src/classnames.ts
+++ src/classnames.ts
@@ -1,17 +1,5 @@
-const ChapterHeader = {
-  container: 'ChapterHeader-container',
-  chapterId: 'ChapterHeader-chapterId',
-  bismillahContainer: 'ChapterHeader-bismillahContainer',
-  translatedName: 'ChapterHeader-translatedName',
-  header: 'ChapterHeader-header',
-  chapterIconContainer: 'ChapterHeader-chapterIconContainer',
-};
-
 const ChapterIconContainer = {
   iconContainer: 'ChapterIconContainer-iconContainer',
-  iconContainerSmall: 'ChapterIconContainer-iconContainerSmall',
-  iconContainerLarge: 'ChapterIconContainer-iconContainerLarge',
-  iconContainerMega: 'ChapterIconContainer-iconContainerMega',
 };
 
 const MushafWord = {
@@ -19,35 +7,6 @@ const MushafWord = {
   colored: 'MushafWord-colored',
   highlightOnHover: 'MushafWord-highlightOnHover',
   highlighted: 'MushafWord-highlighted',
-  'mushaf-hafs': 'MushafWord-mushaf-hafs',
-  'mushaf-shoba': 'MushafWord-mushaf-shoba',
-  'mushaf-warsh': 'MushafWord-mushaf-warsh',
-  'mushaf-qunbul': 'MushafWord-mushaf-qunbul',
-  'mushaf-qaloon': 'MushafWord-mushaf-qaloon',
-  'mushaf-alsoosi': 'MushafWord-mushaf-alsoosi',
-  'mushaf-aldouri': 'MushafWord-mushaf-aldouri',
-  'mushaf-albazzi': 'MushafWord-mushaf-albazzi',
-};
-
-const MushafReader = {
-  infiniteScroll: 'MushafReader-infiniteScroll',
-  readingView: 'MushafReader-readingView',
-  loading: 'MushafReader-loading',
-  container: 'MushafReader-container',
-  withVisibleSideBar: 'MushafReader-withVisibleSideBar',
-  withSidebarNavigationOpenOrAuto: 'MushafReader-withSidebarNavigationOpenOrAuto',
-  playButton: 'MushafReader-playButton',
-};
-
-const Line = {
-  line: 'Line-line',
-  container: 'Line-container',
-  mobileInline: 'Line-mobileInline',
-};
-
-const Page = {
-  container: 'Page-container',
-  mobileCenterText: 'Page-mobileCenterText',
 };
 
 const ReadingView = {
@@ -55,22 +14,12 @@ const ReadingView = {
 };
 
 const VerseText = {
-  verseText: 'VerseText-verseText',
-  verseTextContainer: 'VerseText-verseTextContainer',
-  largeMushafTextLayoutContainer: 'VerseText-largeMushafTextLayoutContainer',
-  largeMushafTextLayout: 'VerseText-largeMushafTextLayout',
-  verseTextCenterAlign: 'VerseText-verseTextCenterAlign',
-  verseTextSpaceBetween: 'VerseText-verseTextSpaceBetween',
   highlighted: 'VerseText-highlighted',
 };
 
 const classnames = {
-  ChapterHeader,
   ChapterIconContainer,
   MushafWord,
-  MushafReader,
-  Line,
-  Page,
   ReadingView,
   VerseText,
 };
