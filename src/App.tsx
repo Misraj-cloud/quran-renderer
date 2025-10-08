f5e6fea0f302f9a87a8916ce47ff3743e6b2136b
khaledhajij <hajij94@gmail.com>
2025-10-08 15:05:41 +0300

    chore: Editing font scales values

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ const MushafPageConsumer: React.FC = () => <Mushaf />;
 const App: React.FC = () => (
   <MushafReaderProvider
     initialIsTwoPagesView
-    initialFontScale={1}
     hasBorder
     pageNumber={221}
     dataId="ar.saoodshuraym.hafs"
