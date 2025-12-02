6d936f04ce8bdb16c22ff0e07fce1a847ed79821
Khaled Ha <hajij94@gmail.com>
2025-12-02 13:56:35 +0300

    bugfix: Adding missing number zero

--- src/components/MushafReader/ReadingView/PageNumber.tsx
+++ src/components/MushafReader/ReadingView/PageNumber.tsx
@@ -16,21 +16,22 @@ const PageNumber = ({ value, borderColor }: Props) => {
   return (
     <div className={styles.pageNumberContainer}>
       <img src={pageNumberBorder[borderColor || 'green']} />
-      <p className={styles.pageNumber}>{pageNumberToPersian(`${value}`)}</p>
+      <p className={styles.pageNumber}>{pageNumberToHindi(`${value}`)}</p>
     </div>
   );
 };
 
 export default PageNumber;
 
-const pageNumberToPersian = (pageNumber: string) => {
+const pageNumberToHindi = (pageNumber: string) => {
   const pageNumbersArr = pageNumber.split('');
   return pageNumbersArr
-    .map((number) => englishToPersian[number as keyof typeof englishToPersian])
+    .map((number) => englishToHindi[number as keyof typeof englishToHindi])
     .join('');
 };
 
-const englishToPersian = {
+const englishToHindi = {
+  '0': '۰',
   '1': '۱',
   '2': '۲',
   '3': '۳',
